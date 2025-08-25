e729c263d0fd7504ce2b3c7fd4f9dd615ca2df31
FranEFabrello <107963067+FranEFabrello@users.noreply.github.com>
2025-08-25 11:39:59 -0300

    refactor cart drawer to use headlessui dialog

--- src/Components/CartDrawer.jsx
+++ src/Components/CartDrawer.jsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,6 +15,7 @@ import { PATHS } from "../routes/paths.js";
 export default function CartDrawer({ open, onClose }) {
   const { items, totalAmount } = useSelector((s) => s.cart);
   const dispatch = useDispatch();
+  const closeButtonRef = useRef(null);
 
   const handleQtyChange = (id, qty) => {
     const quantity = Number(qty);
@@ -22,22 +25,22 @@ export default function CartDrawer({ open, onClose }) {
   };
 
   return (
-    <>
-      <div
-        className={`fixed inset-0 z-30 bg-black/30 transition-opacity ${
-          open ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-        onClick={onClose}
-      />
-      <aside
-        className={`fixed right-0 top-0 z-40 h-full w-64 bg-white p-4 border-l border-gray-200 transform transition-transform ${
+    <Dialog
+      open={open}
+      onClose={onClose}
+      initialFocus={closeButtonRef}
+      className="relative z-40"
+    >
+      <DialogBackdrop className="fixed inset-0 bg-black/30" />
+      <DialogPanel
+        className={`fixed right-0 top-0 h-full w-64 bg-white p-4 border-l border-gray-200 transform transition-transform ${
           open ? "translate-x-0" : "translate-x-full"
         }`}
-        aria-hidden={!open}
       >
         <div className="flex justify-between mb-4">
           <h2 className="font-semibold text-lg">Cart</h2>
           <button
+            ref={closeButtonRef}
             className="p-2 text-gray-700 hover:text-gray-900"
             aria-label="Close cart"
             onClick={onClose}
@@ -106,7 +109,7 @@ export default function CartDrawer({ open, onClose }) {
             </div>
           </div>
         )}
-      </aside>
-    </>
+      </DialogPanel>
+    </Dialog>
   );
 }
