16c26b02b848d227ba1c2a11c168a49278d2eeca
FranEFabrello <frfabrello@uade.edu.ar>
2025-11-16 15:09:55 -0300

    Update API base URL configuration for local development

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://ecommerce-prod-env.eba-guhrbi5i.sa-east-1.elasticbeanstalk.com/api/",
+    baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 api.interceptors.request.use((config) => {
